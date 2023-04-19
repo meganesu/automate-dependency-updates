65feef9388ae7dc5cd8f3884be1b9855c851ee80
Megan Sullivan <meganesu@users.noreply.github.com>
2023-04-18 17:34:52 -0700

    test: add react-testing-library

--- src/Counter.test.jsx
+++ src/Counter.test.jsx
@@ -1,7 +1,11 @@
 import { describe, it, expect } from "vitest"
+import { render } from "@testing-library/react"
+
+import Counter from "./Counter"
 
 describe("Counter", () => {
   it("renders", () => {
+    render(<Counter />)
     expect(true).toBe(true)
   })
 })
